d0515c281611d784d0e47aa2a6e9333f05d3fd4c
G-Conard <conardg@lafayette.edu>
2020-07-24 13:49:27 -0400

    Corrected channel order in moveLeg

--- examples/moveLeg/moveLeg.ino
+++ examples/moveLeg/moveLeg.ino
@@ -9,7 +9,7 @@ float freq = 6;
 //set up for the Right Front leg:
 //function prototype is Leg3d(side, hip_center, femur_center, tibia_center, hip_num, femur_num, tibia_num)
 //side is 2 for left, 1 for right
-Leg3D leg = Leg3D(1, 90,90,90,0,1,2);
+Leg3D leg = Leg3D(1, 90,90,90,2,1,0);
 
 void setup(){
     Serial.begin(115200);
